8e2240e23ec80e94ca1197f7b26ac00a1dc738ad
Cam Holloway <hollowprince@outlook.com>
2018-05-15 17:58:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,3 +31,11 @@ function makeInt(n) {
 function makeInt(n) {
   return parseFloat(n);
 }
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
+
+function preserveDecimal(n) {
+  return parseInt(n);
+}
